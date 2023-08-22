e1c51ab55c9f1f3324d67e4f2aa3895f6c9811b7
Ky Nguyen <kynguyen@Kys-MacBook-Pro-2.local>
2023-08-22 11:28:53 +0700

    Test process sheet data

---
+++ pages/api/sheet.ts
@@ -0,0 +1,39 @@
+import { google } from "googleapis";
+import keys from "../../key.json";
+
+export default function handler(req, res) {
+    console.log("hello in sheet.ts")
+    // return res.status(200).send({ message: "hello"})
+
+    try {
+            console.log("hello in sheet.ts:1")
+
+        const client = new google.auth.JWT(
+            keys.client_email,
+            null,
+            keys.private_key,
+            ['https://www.googleapis.com/auth/spreadsheets']
+        );
+
+        client.authorize(async function(err, tokens) {
+            if (err) {
+                return res.status(400).send(JSON.stringify({error: true}));
+            }
+
+            const gsapi = google.sheets({version:'v4', auth: client});
+
+            //CUSTOMIZATION FROM HERE
+            const opt = {
+                spreadsheetId: '10hdvDYX-UaVEUNlaY1jCHidTDjKeTV_TbkZH0AuT1m8',
+                range: 'Form Responses 1'
+            };
+
+            let data = await gsapi.spreadsheets.values.get(opt);
+            console.log("data", data)
+            return res.status(200).send(JSON.stringify({error: false, data: data.data.values}));
+        });
+    } catch (e) {
+        console.log("error", e)
+        return res.status(400).send(JSON.stringify({error: true, message: e.message}));
+    }
+}
